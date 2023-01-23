4e26831fd4f0c05bdb10548361be14dbc69e7a77
Iryna Melets <irynamelets@gmail.com>
2023-01-23 10:24:02 +0200

    update chessboard

--- l2.js
+++ l2.js
@@ -1,14 +1,11 @@
 function cheessboard(a, b) {
-    for (let l = 1; l <= a; l++) {
-        let p;
-        if (l % 2 == 0) {
-            p = "_#"; 
-        } else {
-            p = "#_";
+    for (let r = 0; r < a; r++) {
+        let line = "";
+        for (let c = 0; c < b; c++) {
+            line += (r + c) % 2 == 0 ? "#" : "_";
         }
-        let line = p.repeat(b / 2);
         console.log(line);
     }
 }
 
-cheessboard(8, 8)
+cheessboard(9, 9)
